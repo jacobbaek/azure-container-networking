3c49839e828f01ea21ef5eb3b0e3917d354a3f50
Hunter Gregory <hunterlgregory@gmail.com>
2023-03-29 15:28:19 -0700

    FIXME: remove tmp commit overriding applyDP config (using for pipeline tests)

--- npm/cmd/start.go
+++ npm/cmd/start.go
@@ -126,6 +126,10 @@ func start(config npmconfig.Config, flags npmconfig.Flags) error {
 		npmV2DataplaneCfg.ApplyDataPlaneMaxCount = config.ApplyDataPlaneMaxCount
 		npmV2DataplaneCfg.ApplyDataPlaneInterval = time.Duration(config.ApplyDataPlaneIntervalInMilliseconds * int(time.Millisecond))
 
+		// FIXME remove this code which overrides Apply config
+		npmV2DataplaneCfg.ApplyDataPlaneMaxCount = 100
+		npmV2DataplaneCfg.ApplyDataPlaneInterval = 500 * time.Millisecond
+
 		if config.WindowsNetworkName == "" {
 			npmV2DataplaneCfg.NetworkName = util.AzureNetworkName
 		} else {
